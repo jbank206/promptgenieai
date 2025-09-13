6280bf41f5d71b96e5b10c8d9b1659285e5eb502
jbank206 <jcpimpin206@aol.com>
2025-09-13 17:35:32 -0500

    Delete metro.config.js

--- metro.config.js
+++
@@ -1,8 +0,0 @@
-const { getDefaultConfig } = require('expo/metro-config');
-
-const config = getDefaultConfig(__dirname);
-
-// Ensure web platform is supported
-config.resolver.platforms = ['ios', 'android', 'native', 'web'];
-
-module.exports = config;
